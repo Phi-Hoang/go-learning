5b5b82481fbeb3091e81c96668515d39ef87b45a
Phi <phihdn@gmail.com>
2019-11-21 15:14:23 +0700

    json to struct

---
+++ json-to-struct/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// Student type
+type Student struct {
+	FirstName string `json:"first_name" bson:"full_name" validate:"required"`
+	LastName  string `json:"last_name"`
+	Age       int    `json:"age"`
+	Class     string `json:"class_name"`
+}
+
+// GetFullName returns the fullname of a student
+func (s Student) GetFullName() string {
+	return s.FirstName + " " + s.LastName
+}
+
+func main() {
+	inputJSON := `[
+		{"first_name": "Victor", "last_name": "Nguyen", "age": 100, "class_name":"golang"},
+		{"first_name": "Anh", "last_name": "Dinh", "age":200, "class_name":"golang"}
+		]`
+	jsonData := []byte(inputJSON)
+	var outStudents []Student
+	err := json.Unmarshal(jsonData, &outStudents)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, st := range outStudents {
+		fmt.Printf("%+v\n", st)
+	}
+}
